a3400826b8504cb63983f320c86fe939bbb48955
Shivam77607 <shivamsharma77607@gmail.com>
2023-04-07 21:31:06 +0530

    pushing data to firebase

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://justaddtocart-default-rtdb.asia-southeast1.firebasedatabase.app/" 
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
 const inputField = document.getElementById("input-field")
 const addButton = document.getElementById("add-button")
 
 addButton.addEventListener("click", () => {
     const inputValue = inputField.value
+
+    push(shoppingListInDB, inputValue)
 })
