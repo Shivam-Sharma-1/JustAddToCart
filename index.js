ec548160902da86318b446011546d62e4db56787
Shivam77607 <shivamsharma77607@gmail.com>
2023-04-07 21:19:30 +0530

    added event listeners

---
+++ index.js
@@ -0,0 +1,6 @@
+const inputField = document.getElementById("input-field")
+const addButton = document.getElementById("add-button")
+
+addButton.addEventListener("click", () => {
+    const inputValue = inputField.value
+})
